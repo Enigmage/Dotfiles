353f1c4e41b2952bffed0feace7112c1f4def5f0
Enigmage <alizaidi0786.az@gmail.com>
2023-07-22 03:47:11 +0530

    update

--- xdg-config/.config/nixos/configuration.nix
+++ xdg-config/.config/nixos/configuration.nix
@@ -99,6 +99,26 @@
     interval = "weekly";
   };
 
+  systemd.timers."home-internal-backup" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      Unit = "home-internal-backup.service";
+    };
+  };
+  systemd.services."home-internal-backup" = {
+    path = [
+      pkgs.borgbackup
+    ];
+    script = ''
+      ${pkgs.bash}/bin/bash /home/alizaidi/scripts/layer1-home-borg.sh
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
   # Enable CUPS to print documents.
   services.printing = {
     enable = true;
