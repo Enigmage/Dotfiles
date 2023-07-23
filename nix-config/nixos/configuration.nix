bf05b24a44eb9c0f0738a510725529b1e0d0503f
Enigmage <alizaidi0786.az@gmail.com>
2023-07-23 23:55:37 +0530

    update

--- nix-config/nixos/configuration.nix
+++ nix-config/nixos/configuration.nix
@@ -205,6 +205,61 @@
     # possible redundant
     libgccjit
     gccgo12
+    # to run generic binaries
+    (pkgs.buildFHSUserEnv {
+      name = "fhs";
+      targetPkgs = pkgs: with pkgs; [
+        alsa-lib
+        atk
+        cairo
+        cups
+        curl
+        dbus
+        expat
+        file
+        zsh
+        fontconfig
+        freetype
+        fuse
+        glib
+        gtk3
+        libGL
+        libnotify
+        libxml2
+        libxslt
+        netcat
+        nspr
+        nss
+        openjdk8
+        openssl.dev
+        pango
+        pkg-config
+        strace
+        udev
+        vulkan-loader
+        watch
+        wget
+        which
+        xorg.libX11
+        xorg.libxcb
+        xorg.libXcomposite
+        xorg.libXcursor
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXrender
+        xorg.libXScrnSaver
+        xorg.libxshmfence
+        xorg.libXtst
+        xorg.xcbutilkeysyms
+        zlib
+        fontconfig.lib
+      ];
+      profile = ''export FHS=1'';
+      runScript = "zsh";
+    })
   ];
   environment.shells = [ pkgs.zsh ];
 
