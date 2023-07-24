fc82ac7588e3ff76174b19f8f66be997ea087428
Enigmage <alizaidi0786.az@gmail.com>
2023-07-24 12:34:12 +0530

    update

--- nix-config/nixos/configuration.nix
+++ nix-config/nixos/configuration.nix
@@ -281,16 +281,10 @@
 
   # power management
   # https://github.com/NixOS/nixpkgs/issues/211345
-  services.thermald.enable = false;
+  services.thermald.enable = true;
   services.power-profiles-daemon.enable = false;
   services.tlp = {
     enable = true;
-    settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    };
   };
 
   # Enable the OpenSSH daemon.
