1f89ac6f0ad6d1510fbf2138c740e4a9a26824c6
Enigmage <alizaidi0786.az@gmail.com>
2023-07-28 01:22:01 +0530

    update

--- nix-config/home-manager/home.nix
+++ nix-config/home-manager/home.nix
@@ -89,40 +89,6 @@
   programs.wezterm.enable = true;
   programs.chromium.enable = true;
   programs.git.enable = true;
-  programs.neovim = {
-    enable = true;
-    package = pkgs.neovim-nightly;
-    plugins = with pkgs.vimPlugins; [
-      packer-nvim
-    ];
-    extraPackages = with pkgs; [
-      luajit
-      tree-sitter
-      python311Packages.pynvim
-      python311Packages.black
-      pyright
-      nodejs
-      gcc
-      gopls
-      deno
-      nil
-      nodePackages.bash-language-server
-      nodePackages.prettier
-      nodePackages.typescript-language-server
-      nodePackages.dockerfile-language-server-nodejs
-      # Web (ESLint, HTML, CSS, JSON)
-      nodePackages.vscode-langservers-extracted
-      lua-language-server
-      texlab
-      nixpkgs-fmt
-      ripgrep
-      fd
-      clang-tools
-      stylua
-      gofumpt
-      beautysh
-    ];
-  };
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
@@ -240,11 +206,6 @@
     ".config/git" = {
       recursive = true;
       source = ./sources/git;
-    };
-
-    ".config/nvim" = {
-      source = ./sources/nvim;
-      recursive = true;
     };
 
     ".config/wezterm" = {
---
+++ nix-config/home-manager/programs/neovim.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }: {
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+    plugins = with pkgs.vimPlugins; [
+      packer-nvim
+    ];
+    extraPackages = with pkgs; [
+      luajit
+      tree-sitter
+      python311Packages.pynvim
+      python311Packages.black
+      pyright
+      nodejs
+      gcc
+      gopls
+      deno
+      nil
+      nodePackages.bash-language-server
+      nodePackages.prettier
+      nodePackages.typescript-language-server
+      nodePackages.dockerfile-language-server-nodejs
+      # Web (ESLint, HTML, CSS, JSON)
+      nodePackages.vscode-langservers-extracted
+      lua-language-server
+      texlab
+      nixpkgs-fmt
+      ripgrep
+      fd
+      clang-tools
+      stylua
+      gofumpt
+      beautysh
+    ];
+  };
+  home.file.".config/nvim" = {
+    source = ../sources/nvim;
+    recursive = true;
+  };
+}
