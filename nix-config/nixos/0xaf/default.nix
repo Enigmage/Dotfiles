d56f010af1b13a7a123f36dac3da1f2cfec91ebc
Enigmage <alizaidi0786.az@gmail.com>
2023-08-16 23:11:48 +0530

    update

--- nix-config/nixos/0xaf/default.nix
+++ nix-config/nixos/0xaf/default.nix
@@ -90,7 +90,7 @@
     # xserver.layout = "us";
     # Enable touchpad support (enabled default in most desktopManager).
     libinput = {
-      enable = true;
+      enable = false;
       touchpad = {
         tapping = true;
         middleEmulation = true;
@@ -100,7 +100,7 @@
       };
     };
     synaptics = {
-      enable = false;
+      enable = true;
       twoFingerScroll = true;
     };
   };
