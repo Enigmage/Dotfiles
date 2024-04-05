c09fd90c47bbdb2e97bd6f14237f3f6f1a4f739e
Enigmage <alizaidi0786.az@gmail.com>
2024-04-05 09:55:15 +0530

    update

--- nix-config/home-manager/0xaf/home.nix
+++ nix-config/home-manager/0xaf/home.nix
@@ -65,16 +65,10 @@
     # awscli2
 
     # gui apps
-    #vlc
-    #firefox
-    # brave
-    # wezterm
-    ## obsidian
-    #vscode
-    #guake
-    #discord
-    #psensor
-    #libreoffice
+    # vlc
+    # vscode
+    # guake
+    # discord
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
